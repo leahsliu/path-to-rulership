ba96b6b486c94cf24e7ad09e904f4ad18454565a
Alexis Liu <aliu41@student.ubc.ca>
2024-06-13 22:45:19 -0700

    idle and down sprite animations

---
+++ Assets/player.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="player" tilewidth="3" tileheight="3" tilecount="15360" columns="96">
+ <image source="../../../Downloads/mystic_woods_free_2.2/sprites/characters/player.png" width="288" height="480"/>
+</tileset>
