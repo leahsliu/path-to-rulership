d1fee4b8a79365cb2bfc01c6eb848545e59e6aba
Leah Liu <leahliu@Leahs-MacBook-Pro.local>
2024-06-15 23:41:30 -0700

    modified render, added velocity to update + boundaries

--- scripts/entities.py
+++ scripts/entities.py
@@ -7,14 +7,20 @@ class Entities:
         self.type = e_type
         self.pos = list(pos)
         self.size = size
-        # self.velocity = [0,0]
+        self.velocity = [0,0]
 
 
     def update(self, movement=(0,0)):
-        # dx, dy = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
-        dx, dy = movement
+        dx, dy = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+        # dx, dy = movement
         self.pos[0] += dx
         self.pos[1] += dy
 
+        # Boundary check to keep entity within screen
+        self.pos[0] = max(0, min(self.pos[0], self.game.display.get_width() - self.size[0]))
+        self.pos[1] = max(0, min(self.pos[1], self.game.display.get_height() - self.size[1]))
+        print(f"Player position: {self.pos}, Velocity: {self.velocity}")  # Debugging line
+
+
     def render(self, display):
-        display.blit(self.game.assets['player-d1'], self.pos)
+        display.blit(self.game.assets[self.type], self.pos)
